b096747eb9983976ae84618f69ee16d3010b5400
Phil Helmkamp <phelmkamp@gmail.com>
2022-04-18 16:27:33 -0400

    move "value" pkg to "optional"
     * deprecate "value" pkg

--- tuple/four/four_tuple.go
+++ tuple/four/four_tuple.go
@@ -6,8 +6,8 @@
 package four
 
 import (
+	"github.com/phelmkamp/valor/optional"
 	"github.com/phelmkamp/valor/result"
-	"github.com/phelmkamp/valor/value"
 )
 
 // Tuple contains four values.
@@ -23,11 +23,11 @@ func TupleOf[T, T2, T3, T4 any](v T, v2 T2, v3 T3, v4 T4) Tuple[T, T2, T3, T4] {
 	return Tuple[T, T2, T3, T4]{V: v, V2: v2, V3: v3, V4: v4}
 }
 
-// TupleValueOf creates a value.Value of (v, v2, v3, v4) if ok is true.
+// TupleValueOf creates a optional.Value of (v, v2, v3, v4) if ok is true.
 // This aids interoperability with return values
 // that follow the "comma ok" idiom.
-func TupleValueOf[T, T2, T3, T4 any](v T, v2 T2, v3 T3, v4 T4, ok bool) value.Value[Tuple[T, T2, T3, T4]] {
-	return value.Of(TupleOf(v, v2, v3, v4), ok)
+func TupleValueOf[T, T2, T3, T4 any](v T, v2 T2, v3 T3, v4 T4, ok bool) optional.Value[Tuple[T, T2, T3, T4]] {
+	return optional.Of(TupleOf(v, v2, v3, v4), ok)
 }
 
 // TupleResultOf creates a result.Result of either (v, v2, v3, v4) or err.
--- tuple/three/three_tuple.go
+++ tuple/three/three_tuple.go
@@ -6,8 +6,8 @@
 package three
 
 import (
+	"github.com/phelmkamp/valor/optional"
 	"github.com/phelmkamp/valor/result"
-	"github.com/phelmkamp/valor/value"
 )
 
 // Tuple contains three values.
@@ -22,11 +22,11 @@ func TupleOf[T, T2, T3 any](v T, v2 T2, v3 T3) Tuple[T, T2, T3] {
 	return Tuple[T, T2, T3]{V: v, V2: v2, V3: v3}
 }
 
-// TupleValueOf creates a value.Value of (v, v2, v3) if ok is true.
+// TupleValueOf creates a optional.Value of (v, v2, v3) if ok is true.
 // This aids interoperability with return values
 // that follow the "comma ok" idiom.
-func TupleValueOf[T, T2, T3 any](v T, v2 T2, v3 T3, ok bool) value.Value[Tuple[T, T2, T3]] {
-	return value.Of(TupleOf(v, v2, v3), ok)
+func TupleValueOf[T, T2, T3 any](v T, v2 T2, v3 T3, ok bool) optional.Value[Tuple[T, T2, T3]] {
+	return optional.Of(TupleOf(v, v2, v3), ok)
 }
 
 // TupleResultOf creates a result.Result of either (v, v2, v3) or err.
