ae96e0e274deb230387918e25e0f7af59ba0a23c
phelmkamp <phelmkamp@gmail.com>
2022-05-07 09:02:29 -0400

    add copyright to funcs.go

--- funcs/funcs.go
+++ funcs/funcs.go
@@ -1,3 +1,7 @@
+// Copyright 2022 phelmkamp. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
 // Package funcs provides common functions that
 // can be used as arguments to map, zip, etc.
 package funcs
