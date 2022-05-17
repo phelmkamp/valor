fecf547b3615d9c5865e768ef90e58e98ece996e
Phil <phelmkamp@gmail.com>
2022-05-16 22:19:19 -0400

    valorcheck: handle enum (#26)

--- valorcheck/testdata/main.go
+++ valorcheck/testdata/main.go
@@ -5,16 +5,33 @@
 package main
 
 import (
+	"strings"
+
+	"github.com/phelmkamp/valor/enum"
 	"github.com/phelmkamp/valor/optional"
+	"github.com/phelmkamp/valor/result"
+)
+
+var (
+	Fruit = enum.OfString("apple", "banana", "orange")
 )
 
 func main() {
+	var w strings.Builder
 	m := map[string]int{"foo": 1}
 	val := optional.OfIndex(m, "foo")
+	res := result.Of(w.Write(nil))
+	f := Fruit.ValueOf("apple")
 
-	optional.OfIndex(m, "foo").MustOk() // UNGUARDED
-	i := val.MustOk()                   // UNGUARDED
-	val.Ok(&i)                          // UNCHECKED
+	optional.OfIndex(m, "foo").MustOk()      // UNGUARDED
+	i := val.MustOk()                        // UNGUARDED
+	result.Of(w.Write(nil)).Value().MustOk() // UNGUARDED
+	n := res.Value().MustOk()                // UNGUARDED
+	Fruit.ValueOf("apple").MustOk()          // UNGUARDED
+	s := f.ValueOf("apple").MustOk()         // UNGUARDED
+	val.Ok(&i)                               // UNCHECKED
+	res.Value().Ok(&n)                       // UNCHECKED
+	f.Ok(&s)                                 // UNCHECKED
 
 	if val.IsOk() {
 		i = val.MustOk()
@@ -35,5 +52,11 @@ func main() {
 
 	if n := 0; val.Ok(&n) {
 		n++
+	}
+
+	switch val := optional.Of(0, true); val {
+	case val.OfOk():
+		i = val.MustOk()
+	case optional.OfNotOk[int]():
 	}
 }
